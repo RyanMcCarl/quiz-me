3dea86290ffa27a0e0bb0dc2bad2024b7659bdf0
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2011-01-15 18:18:37 -0600

    Improvements to the main activities buttons.
    They have better naming and actually listen to clicks now.
    
    Each buttons click launches it's appropriate activity.

--- src/com/marzhillstudios/quizme/MainActivity.java
+++ src/com/marzhillstudios/quizme/MainActivity.java
@@ -7,7 +7,12 @@
 package com.marzhillstudios.quizme;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * 
@@ -18,9 +23,30 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Context mainCtx = this;
+        Button quizLaunchBtn = (Button)findViewById(R.id.LaunchQuizBtn);
+        Button managerLaunchBtn = (Button)findViewById(R.id.launchManagerBtn);
+        
+        OnClickListener quizBtnListener = new OnClickListener() {
+			
+			public void onClick(View v) {
+				startActivity(new Intent(mainCtx, QuizActivity.class));
+			}
+		};
+		
+		OnClickListener manageBtnListener = new OnClickListener() {
+			
+			public void onClick(View v) {
+				startActivity(new Intent(mainCtx, CardManagerActivity.class));
+			}
+		};
+		
+		quizLaunchBtn.setOnClickListener(quizBtnListener);
+		managerLaunchBtn.setOnClickListener(manageBtnListener);
     }
 }
