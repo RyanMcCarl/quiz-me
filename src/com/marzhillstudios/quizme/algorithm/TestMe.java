dfc2a6bf98204db4812b1df17bfac18f4efd79b2
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2011-01-17 16:32:18 -0600

    The class that will contain the SpacedRepetition algorithm.

---
+++ src/com/marzhillstudios/quizme/algorithm/TestMe.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2011 Jeremy Wall <jeremy@marzhillstudios.com>
+ * All contents Licensed under the terms of the Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+package com.marzhillstudios.quizme.algorithm;
+
+/**
+ * TODO:Class Description here.
+ *
+ * @author Jeremy Wall <jeremy@marzhillstudios.com>
+ *
+ */
+
+public class TestMe {
+
+}
+
