23caedcbd545fe08ca8c06ef0e8c710ba1f32347
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2011-01-18 22:10:49 -0600

    mostly full test for the launchQuizBtn press.

--- test/src/com/marzhillstudios/quizme/MainActivityTest.java
+++ test/src/com/marzhillstudios/quizme/MainActivityTest.java
@@ -9,8 +9,11 @@ package com.marzhillstudios.quizme;
 import com.marzhillstudios.quizme.R;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.ViewAsserts;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -28,6 +31,9 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     protected Activity activity;
     protected Button launchQuizBtn;
     protected Button launchCardManagerBtn;
+    protected View mainLayout;
+    protected View quizLayout;
+    protected View managerLayout;
     
     public MainActivityTest() {
         super("com.marzhillstudios.quizme", MainActivity.class);
@@ -38,11 +44,30 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         activity = this.getActivity();
         launchQuizBtn = (Button) activity.findViewById(R.id.LaunchQuizBtn);
         launchCardManagerBtn = (Button) activity.findViewById(R.id.launchManagerBtn);
+        mainLayout = (View) activity.findViewById(R.id.MainLayout);
+        quizLayout = (View) activity.findViewById(R.id.QuizLayout);
+        managerLayout = (View) activity.findViewById(R.id.ManagerLayout);
         
     }
 
     protected void testPreconditions() throws Exception {
+        assertNotNull(mainLayout);
         assertNotNull(launchQuizBtn);
         assertNotNull(launchCardManagerBtn);
+    }
+
+    protected void testLaunchQuizBtnLaunchesQuizActivity() {
+        // TODO(jwall): figure out how to actually test this.
+        activity.runOnUiThread(
+          new Runnable() {
+              public void run() {
+                  launchQuizBtn.requestFocus();
+              }
+          }
+        );
+
+        TouchUtils.clickView(this, launchQuizBtn);
+        // TODO(jwall): check that QuizActivity has started.
+        ViewAsserts.assertOnScreen(launchQuizBtn, quizLayout); 
     }
 }
