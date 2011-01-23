1b599b54338bd1a3d650674e3508084a05236141
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2011-01-22 20:10:30 -0600

    refactored dialog into a custom activity. (Compiles but not tested)
    I had to use an activity instead because the callback options
    for a dialog were two limited.
    
    I still need to implement the onActivityResult callback to handle the
    various edit actions that can occur when creating a card.

--- src/com/marzhillstudios/quizme/CardManagerActivity.java
+++ src/com/marzhillstudios/quizme/CardManagerActivity.java
@@ -7,15 +7,14 @@
 package com.marzhillstudios.quizme;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.widget.Button;
-import android.widget.ListView;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
-import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.marzhillstudios.quizme.adapter.CardListAdapter;
 import com.marzhillstudios.quizme.data.CardDatabase;
@@ -35,8 +34,6 @@ public class CardManagerActivity extends Activity {
     private CardListAdapter listAdapter;
     private ListView listView;
 
-    public static final int DIALOG_NEW_CARD = 1;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,79 +47,13 @@ public class CardManagerActivity extends Activity {
 
         OnClickListener newCardListener = new OnClickListener() {
             public void onClick(View v) {
-                showDialog(DIALOG_NEW_CARD);
+                // TODO(jwall): start the NewCardCreatorActivity
+                // instead
+                //showDialog(DIALOG_NEW_CARD);
             }
         };
 
         newCardBtn.setOnClickListener(newCardListener);
-    }
-
-    protected Dialog onCreateDialog(int id) {
-        Dialog dialog;
-        switch(id) {
-            case DIALOG_NEW_CARD:
-                dialog = createNewCardDialog();
-                break;
-            default:
-                dialog = null;
-        }
-        return dialog;
-    }
-
-    public Dialog createNewCardDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater =
-            (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        ViewGroup layoutRoot =
-            (ViewGroup) findViewById(R.id.NewCardLayout);
-        builder.setView(inflater.inflate(R.layout.new_card_dialog, layoutRoot));
-        AlertDialog dialog = builder.create();
-
-        Button side1ImageBtn =
-            (Button) dialog.findViewById(R.id.NewCardSide1ImageBtn);
-
-        OnClickListener side1ImageBtnListener = new OnClickListener() {
-            public void onClick(View v) {
-                // TODO(jwall): we need to start an activity to get an image.
-            }
-        };
-
-        side1ImageBtn.setOnClickListener(side1ImageBtnListener);
-
-        Button side1TextBtn =
-            (Button) dialog.findViewById(R.id.NewCardSide1TextBtn);
-
-        OnClickListener side1TextBtnListener = new OnClickListener() {
-            public void onClick(View v) {
-                // TODO(jwall): we need to start an activity to write text.
-            }
-        };
-
-        side1TextBtn.setOnClickListener(side1TextBtnListener);
-
-        Button side2ImageBtn =
-            (Button) dialog.findViewById(R.id.NewCardSide2ImageBtn);
-
-        OnClickListener side2ImageBtnListener = new OnClickListener() {
-            public void onClick(View v) {
-                // TODO(jwall): we need to start an activity to get an image.
-            }
-        };
-
-        side2ImageBtn.setOnClickListener(side2ImageBtnListener);
-
-        Button side2TextBtn =
-            (Button) dialog.findViewById(R.id.NewCardSide2TextBtn);
-
-        OnClickListener side2TextBtnListener = new OnClickListener() {
-            public void onClick(View v) {
-                // TODO(jwall): we need to start an activity to write text.
-            }
-        };
-
-        side2TextBtn.setOnClickListener(side2TextBtnListener);
-
-        return dialog;
     }
 
     public CardListAdapter getListAdapter() {
